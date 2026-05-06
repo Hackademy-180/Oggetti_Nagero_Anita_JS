b435af7221be9bf64ff60f38338a3ca1b62ae934
anita <nageroa6@gmail.com>
2026-05-06 16:58:36 +0200

    lavoro terminato

---
+++ main.js
@@ -0,0 +1,9 @@
+
+// Selfwork Oggetti 1
+// Crea un oggetto persona con le seguenti caratteristiche:
+// nome
+// cognome
+// eta'
+// un metodo che permetta di presentarti
+
+// Output: "Ciao, sono nome cognome ed ho età anni"
